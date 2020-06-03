a54cb2cbba8d274d9f3fca11e971f30086b08ab4
Michał Dolaś <me@shockah.pl>
2020-06-03 20:44:35 +0200

    Returning the ComponentId in all Container.register methods

--- dependency/src/main/kotlin/pl/shockah/unikorn/dependency/Container.kt
+++ dependency/src/main/kotlin/pl/shockah/unikorn/dependency/Container.kt
@@ -13,8 +13,9 @@ class Container(
 		return (componentTypeToStorage[id] as? ComponentStorage<T>)?.component ?: parent?.resolve(id) ?: throw MissingComponentException()
 	}
 
-	fun <T: Any, Key> register(id: ComponentId<T, Key>, componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory, factory: (Resolver) -> T) {
+	fun <T: Any, Key> register(id: ComponentId<T, Key>, componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory, factory: (Resolver) -> T): ComponentId<T, Key> {
 		componentTypeToStorage[id] = componentStorageFactory.createComponentStorage(this, factory)
+		return id
 	}
 
 	fun unregister(id: ComponentId<*, *>) {
@@ -22,23 +23,23 @@ class Container(
 	}
 }
 
-fun <T: Any, Key> Container.register(id: ComponentId<T, Key>, component: T, componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory) {
-	register(id, componentStorageFactory) { component }
+fun <T: Any, Key> Container.register(id: ComponentId<T, Key>, component: T, componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory): ComponentId<T, Key> {
+	return register(id, componentStorageFactory) { component }
 }
 
-fun <T: Any> Container.register(type: KClass<T>, componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory, factory: (Resolver) -> T) {
-	register(ComponentId(type), componentStorageFactory, factory)
+fun <T: Any> Container.register(type: KClass<T>, componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory, factory: (Resolver) -> T): ComponentId<T, Unit> {
+	return register(ComponentId(type), componentStorageFactory, factory)
 }
 
-fun <T: Any> Container.register(type: KClass<T>, component: T, componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory) {
-	register(type, componentStorageFactory) { component }
+fun <T: Any> Container.register(type: KClass<T>, component: T, componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory): ComponentId<T, Unit> {
+	return register(type, componentStorageFactory) { component }
 }
 
-inline fun <reified T: Any> Container.register(componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory, noinline factory: (Resolver) -> T) {
+inline fun <reified T: Any> Container.register(componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory, noinline factory: (Resolver) -> T): ComponentId<T, Unit> {
 	return register(T::class, componentStorageFactory, factory)
 }
 
-inline fun <reified T: Any> Container.register(component: T, componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory) {
+inline fun <reified T: Any> Container.register(component: T, componentStorageFactory: ComponentStorageFactory = defaultComponentStorageFactory): ComponentId<T, Unit> {
 	return register(T::class, component, componentStorageFactory)
 }
 
